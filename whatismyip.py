728fd7fb52db5cbaa30afac29bf7447c96030375
mahdi-rfz <mahdifeyzolahy@gmail.com>
2024-06-21 02:33:16 +0330

    add ip receive function

--- whatismyip.py
+++ whatismyip.py
@@ -2,3 +2,19 @@ import requests
 import colorama
 import os 
 
+def ip_receive():
+    try :
+        request_url = 'https://geolocation-db.com/jsonp/' 
+        response = requests.get(request_url)
+        response = response.text
+        response = response.replace("callback(" , "")
+        response = response.replace(")" , "")
+        response = response.replace("null" , "False")
+        
+        return eval(response)
+    
+    except OSError :
+        return " Not found"
+    
+
+print(ip_receive())
