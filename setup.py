19e9057c1b07610593506a95e468e1f42542b47f
mahdi-rfz <mahdifeyzolahy@gmail.com>
2024-06-21 05:12:55 +0330

    add comment

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ run as sudo
 
 you can use this script on unix base os
 """
-
+##add pip installer
 current_dir = input("Enter current directory :")
 script = ("cp whatismyip /bin")
 os.system(script)
