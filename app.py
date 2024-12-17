b2afd8679aa8d94e5be07b4757fbf636d3154dd5
mahdi-rfz <mahdifeyzolahy@gmail.com>
2024-12-18 00:12:58 +0330

    add loger and bind host ip with socket

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 from flask import Flask , request , jsonify
+import socket
+import server_modulus.loger as loger
 
 app = Flask(__name__)
 
 @app.route("/whatismyip" , methods = ["GET" , "POST"])
 def whatIsMyIp():
+    loger.loger(request.remote_addr , request.user_agent)
     return jsonify({"ip":request.remote_addr})
 
+
+
 if (__name__) == ("__main__"):
-    app.run(debug=True , host="localhost" , port=6263)
+    app.run(host = socket.gethostbyname(socket.gethostname()) , port = 6263)
