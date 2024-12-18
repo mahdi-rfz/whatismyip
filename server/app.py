92bbc49c0035db71ef55796a55565297accc61c8
mahdi-rfz <mahdifeyzolahy@gmail.com>
2024-12-18 19:20:53 +0330

    add returnip route

--- server/app.py
+++ server/app.py
@@ -1,6 +1,18 @@
-from flask import Flask , request , jsonify
+from flask import Flask , request , jsonify , render_template
 import socket
+import requests
+import json
 import server_modulus.loger as loger
+
+
+
+
+def checkIpInfo(clientIp):
+        info = requests.get(f"http://ip-api.com/json/{clientIp}")
+        return json.loads((info.text))
+
+
+
 
 app = Flask(__name__)
 
@@ -8,6 +20,16 @@ app = Flask(__name__)
 def whatIsMyIp():
     loger.loger(request.remote_addr , request.user_agent)
     return jsonify({"ip":request.remote_addr})
+
+
+@app.route("/whatismyip/returnip" , methods = ["GET"])
+def whatismyip():
+    loger.loger(request.remote_addr , request.user_agent , request.base_url)
+
+    infq = checkIpInfo((request.remote_addr))
+    return render_template("home.html" , ip = request.remote_addr ,country = infq["country"] ,
+                            city = infq["city"] , isp =infq["isp"])
+
 
 
 
--- server/server_modulus/loger.py
+++ server/server_modulus/loger.py
@@ -1,10 +1,10 @@
 import datetime
 
 
-def loger(ip , userAgent):
+def loger(ip , userAgent , url):
     try : 
         with open("whatismyip-logfile.txt" , "a") as file :
-            file.write(f"\nIP : {ip} , TIME : {datetime.datetime.now()} , User_Agent : {userAgent}")
+            file.write(f"\nIP : {ip} , TIME : {datetime.datetime.now()} , User_Agent : {userAgent} , URL : {url}")
             file.close
             return True
     except Exception : 
